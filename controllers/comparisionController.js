84c7c50d1361c78e33be16c01a3ba23f1d7a3727
Mudit Sharma <muditshar001@gmail.com>
2023-11-21 17:45:27 +0530

    Update comparisionController.js

--- controllers/comparisionController.js
+++ controllers/comparisionController.js
@@ -4,66 +4,60 @@ const User = require('../models/userModel');
 // Get values where 'age' is equal to 20
 const eq = async (req, res) => {
   try {
-    // TODO: Define the logic to find users with 'age' equal to 20
-    // TODO: Respond with the found users
+    const users = await User.find({ age: 20 });
+    res.status(200).json(users);
   } catch (error) {
-    // Handle errors by responding with an appropriate status code and error message
-    res.status(404).send(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
 // Get values where 'age' is greater than 20
 const gt = async (req, res) => {
   try {
-    // TODO: Define the logic to find users with 'age' greater than 20
-    // TODO: Respond with the found users
+    const users = await User.find({ age: { $gt: 20 } });
+    res.status(200).json(users);
   } catch (error) {
-    // Handle errors by responding with an appropriate status code and error message
-    res.status(404).send(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
 // Get values where 'age' is greater than or equal to 20
 const gte = async (req, res) => {
   try {
-    // TODO: Define the logic to find users with 'age' greater than or equal to 20
-    // TODO: Respond with the found users
+    const users = await User.find({ age: { $gte: 20 } });
+    res.status(200).json(users);
   } catch (error) {
-    // Handle errors by responding with an appropriate status code and error message
-    res.status(404).send(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
 // Get values where 'age' is less than 20
 const lt = async (req, res) => {
   try {
-    // TODO: Define the logic to find users with 'age' less than 20
-    // TODO: Respond with the found users
+    const users = await User.find({ age: { $lt: 20 } });
+    res.status(200).json(users);
   } catch (error) {
-    // Handle errors by responding with an appropriate status code and error message
-    res.status(404).send(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
 // Get values where 'age' is less than or equal to 20
 const lte = async (req, res) => {
   try {
-    // TODO: Define the logic to find users with 'age' less than or equal to 20
-    // TODO: Respond with the found users
+    const users = await User.find({ age: { $lte: 20 } });
+    res.status(200).json(users);
   } catch (error) {
-    // Handle errors by responding with an appropriate status code and error message
-    res.status(404).send(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
 // Get values where 'age' includes 20 or 25
 const inop = async (req, res) => {
   try {
-    // TODO: Define the logic to find users with 'age' including 20 or 25
-    // TODO: Respond with the found users
+    const users = await User.find({ age: { $in: [20, 25] } });
+    res.status(200).json(users);
   } catch (error) {
-    // Handle errors by responding with an appropriate status code and error message
-    res.status(404).send(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
